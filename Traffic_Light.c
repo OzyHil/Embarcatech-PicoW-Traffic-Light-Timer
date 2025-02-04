06478301ec4ff1cdc214c64311e797b931c8d82d
Hilquias Rodrigues de Oliveira <eaeamigo2292@gmail.com>
2025-02-03 23:39:47 -0300

    Implement traffic light control with repeating timer

--- Traffic_Light.c
+++ Traffic_Light.c
@@ -1,30 +1,67 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-#define RED_LED 11
-#define YELLOW_LED 12
-#define GREEN_LED 13
+#define RED_LED 11 // Pino do LED vermelho
+#define YELLOW_LED 12 // Pino do LED amarelo
+#define GREEN_LED 13 // Pino do LED verde
 
-void SetOutput(int pin)
+int8_t count = 1; // Variável de controle para alternar entre os LEDs
+
+// Função para inicializar os pinos dos LEDs
+void SetOutputs(int pins[], size_t size)
 {
-    gpio_init(pin);
-    gpio_set_dir(pin, GPIO_OUT);
+    // Loop para inicializar cada pino
+    for (size_t i = 0; i < size; i++)
+    {
+        gpio_init(pins[i]);
+        gpio_set_dir(pins[i], GPIO_OUT); // Define a direção do pino como saída
+    }
+}
+
+// Função de call-back, chamada pelo temporizador a cada intervalo
+bool TrafficLightUpdate(struct repeating_timer *t)
+{
+    // Controla os LEDs de acordo com o valor de 'count'
+    switch (count)
+    {
+    case 0: // Sinal vermelho
+        gpio_put(GREEN_LED, false); // Desliga o LED verde
+        gpio_put(RED_LED, true);    // Liga o LED vermelho
+        break;
+    case 1: // Sinal amarelo
+        gpio_put(RED_LED, false);   // Desliga o LED vermelho
+        gpio_put(YELLOW_LED, true); // Liga o LED amarelo
+        break;
+    case 2: // Sinal verde
+        gpio_put(YELLOW_LED, false); // Desliga o LED amarelo
+        gpio_put(GREEN_LED, true);   // Liga o LED verde
+        count = -1;                  // Reinicia o ciclo de contagem
+        break;
+    }
+
+    count++; // Incrementa o contador para a próxima transição dos sinais
+
+    return true; // Retorna true para continuar chamando o temporizador
 }
 
 int main()
 {
-    stdio_init_all();
+    stdio_init_all(); // Inicializa a biblioteca padrão de entrada/saída
 
-    SetOutput(RED_LED);
-    SetOutput(YELLOW_LED);
-    SetOutput(GREEN_LED);
+    // Inicializa todos os LEDs
+    int led_pins[] = {RED_LED, YELLOW_LED, GREEN_LED};
+    SetOutputs(led_pins, sizeof(led_pins) / sizeof(led_pins[0]));
+
+    // Inicializa o LED vermelho (inicia com o sinal vermelho)
+    gpio_put(RED_LED, true);
+
+    // Configura o temporizador para chamar a função 'TrafficLightUpdate' a cada 3 segundos
+    struct repeating_timer timer;
+    add_repeating_timer_ms(3000, TrafficLightUpdate, NULL, &timer);
+
 
     while (true)
     {
-        gpio_put(RED_LED, true);
-        sleep_ms(1000);
-
-        gpio_put(RED_LED, false);
-        sleep_ms(1000);
+        sleep_ms(1000000);
     }
 }
