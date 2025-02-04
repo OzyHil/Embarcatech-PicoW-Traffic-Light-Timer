97f83d20d19d05bc3de560791706fcb08bf5d65b
Hilquias Rodrigues de Oliveira <eaeamigo2292@gmail.com>
2025-02-03 22:39:55 -0300

    LED configuration and initialization

--- Traffic_Light.c
+++ Traffic_Light.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+#define RED_LED 11
+#define YELLOW_LED 12
+#define GREEN_LED 13
 
+void SetOutput(int pin)
+{
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_OUT);
+}
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
+    SetOutput(RED_LED);
+    SetOutput(YELLOW_LED);
+    SetOutput(GREEN_LED);
+
+    while (true)
+    {
+        gpio_put(RED_LED, true);
+        sleep_ms(1000);
+
+        gpio_put(RED_LED, false);
         sleep_ms(1000);
     }
 }
